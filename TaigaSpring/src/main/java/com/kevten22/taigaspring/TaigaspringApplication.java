87805ee6db2b8091459b7bf1c4eea023f022baac
Kevin Tena <kevten09@gmail.com>
2019-03-04 15:26:20 -0600

    Init

---
+++ TaigaSpring/src/main/java/com/kevten22/taigaspring/TaigaspringApplication.java
@@ -0,0 +1,13 @@
+package com.kevten22.taigaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaigaspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaigaspringApplication.class, args);
+    }
+
+}
