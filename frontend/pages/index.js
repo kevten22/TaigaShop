54db0d60febcbd916d87e222dc1cb2f32fb4571e
Kevin Tena <kevten09@gmail.com>
2019-04-05 21:13:09 -0500

    Converted About Page link into button

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Index = () => (
     <div>
     <Link href="/about" title="About Page">
-    <a> About Page </a>
+    <button> About Page </button>
     </Link>
       <p>Hello Next.js</p>
     </div>
