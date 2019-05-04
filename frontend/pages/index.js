13682aa44ee0a6fa9ce603a5cd600b6f55fbfd98
Kevin Tena <kevten09@gmail.com>
2019-05-03 19:44:28 -0500

    Connected the front end to my Spring server for data and uploaded products to the front page that contain dummy data

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,17 +2,27 @@ import Layout from '../components/MyLayout.js'
 import fetch from 'isomorphic-unfetch'
 
 
-const Index = () => (
+const Index = (props) => (
     <Layout>
       <h1>Welcome to Taiga</h1>
+      <div>
+        {props.products.map(product=> (
+          <div>
+            <p>{product.productname}</p>
+            <p>{product.productprice}</p>
+            <img src={product.imageurl}></img>
+          </div>
+        ))}
+      </div>
     </Layout>
   )
 
   Index.getInitialProps = async function() {
-    //const res = await fetch('placeholderurl')
-    //const data = await res.json()
+    const res = await fetch('http://localhost:2019/products/')
+    const data = await res.json()
 
     return {
+      products: data
     }
   }
   
